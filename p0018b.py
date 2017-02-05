06a1388a5025b61014d1b91fcaae790987577f18
papaikristof <e16.papai.kristof@palfy-vizugyi.hu>
2017-02-05 14:54:59 +0100

    pythonidomar p0018b feladata

---
+++ p0018b.py
@@ -0,0 +1,6 @@
+foglalkozások = ['postás', 'rendőr', 'villanyszerelő', 'a szomszéd', 'gázos', 
+                 'díjbeszedő', 'handlé', 'szódás', 'képkereskedő', 'a házmester', 
+                 'a fia', 'kéményseprő']
+print(foglalkozások[-3])
+print(foglalkozások[-7])
+print(foglalkozások[-12])
