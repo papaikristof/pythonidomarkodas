a60c273d00479ca5ee99d96a5b9adeb35600f850
papaikristof <e16.papai.kristof@palfy-vizugyi.hu>
2017-02-05 15:15:17 +0100

    Add files via upload

---
+++ p0018d.py
@@ -0,0 +1,6 @@
+foglalkozások = ['postás', 'rendőr', 'villanyszerelő', 'a szomszéd', 'gázos', 
+                 'díjbeszedő', 'handlé', 'szódás', 'képkereskedő', 'a házmester', 
+                 'a fia', 'kéményseprő']
+print(foglalkozások[-3:-5])
+print(foglalkozások[-3:-10])
+print(foglalkozások[-2:-8])
---
+++ p0018e.py
@@ -0,0 +1,8 @@
+foglalkozások = ['postás', 'rendőr', 'villanyszerelő', 'a szomszéd', 'gázos', 
+                 'díjbeszedő', 'handlé', 'szódás', 'képkereskedő', 'a házmester', 
+                 'a fia', 'kéményseprő']
+print(foglalkozások[-2:-7:-2])
+print(foglalkozások[-3:-10:-3])
+print(foglalkozások[3:11:3])
+print(foglalkozások[-6:-11:-3])
+print(foglalkozások[2:12:2])
