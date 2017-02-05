e9a0bc19d73c4f7cfd242573eda9bdba385c42dd
papaikristof <e16.papai.kristof@palfy-vizugyi.hu>
2017-02-05 14:51:46 +0100

    Pythonidomar p0018a feladat

---
+++ p0018a.py
@@ -0,0 +1,6 @@
+foglalkozások = ['postás', 'rendőr', 'villanyszerelő', 'a szomszéd', 'gázos', 
+                 'díjbeszedő', 'handlé', 'szódás', 'képkereskedő', 'a házmester', 
+                 'a fia', 'kéményseprő']
+print(foglalkozások[2])
+print(foglalkozások[8])
+print(foglalkozások[6])
