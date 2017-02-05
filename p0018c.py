865abcdc95b92ca8cc4a5d9a3fc092cdbcc4f51e
papaikristof <e16.papai.kristof@palfy-vizugyi.hu>
2017-02-05 14:59:54 +0100

    Add files via upload

---
+++ p0018c.py
@@ -0,0 +1,6 @@
+foglalkozások = ['postás', 'rendőr', 'villanyszerelő', 'a szomszéd', 'gázos', 
+                 'díjbeszedő', 'handlé', 'szódás', 'képkereskedő', 'a házmester', 
+                 'a fia', 'kéményseprő']
+print(foglalkozások[5:8])
+print(foglalkozások[7:11])
+print(foglalkozások[3:6])
